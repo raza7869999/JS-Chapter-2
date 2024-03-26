e8913ea977e0070a365f0b3dd683e19d5dc3ca76
Muhammad Hasnain Raza <157500053+raza7869999@users.noreply.github.com>
2024-03-26 10:48:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+// Q:1
+
+var userName 
+
+//Q:2
+
+var myName = ("Muhammad Hasnain Raza");
+console.log ( myName)
+
+//Q:3
+
+var tittled = ("Hello World");
+alert ("Hello World")
+console.log (tittled)
+
+
+//Q:4
+
+ var studentBioData = ("Jhone Done, 15 years old, Certified Mobile Application Development");
+ console.log (studentBioData)
+
+ alert ("Jhone Done")
+ alert ("15 years old")
+ alert ("Certified Mobile Application Development")
+
+//Q:5
+
+alert ("Pizza \n Pizz \n Piz \n Pi \n P")
+
+//Q:6
+
+var email = ("example@example.com");
+console.log (email)
+alert ("My email address is example@example.com")
+
+//Q:7
+
+var book = ("A smarter way to learn JavaScript")
+console.log ( book)
+alert ("I am trying to learn from the Book A smarter way to learn JavaScript")
+
+
+//Q:9
+
+ var end = ("___________~~##~~__________")
+console.log (end)
+alert ("___________~~##~~__________")
+
+
